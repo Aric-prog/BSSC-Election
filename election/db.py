3f4e72fc07e236d0e8f74ee4b6bdf7714f69e8ab
Alexander <54830847+irnsonde@users.noreply.github.com>
2021-05-13 00:59:52 +0700

    create base db

--- election/db.py
+++ election/db.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import os
 
 app = Flask(__name__)
@@ -7,15 +8,76 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 
 db = SQLAlchemy(app)
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    addresses = db.relationship('Address', backref='person', lazy=True, uselist=False)
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name    = db.Column(db.String(100), nullable=False)
+    NIM     = db.Column(db.String(15), nullable=False)
+    POB     = db.Column(db.String(50), nullable=False)
+    DOB     = db.Column(db.String(50), nullable=False)
+    email   = db.Column(db.String(50), nullable=False)
+    vote    = db.Column(db.Integer, nullable=False)
+    password= db.Column(db.String(50), nullable=False)
 
-class Address(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), nullable=False)
-    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False, unique=True)
+    # Foreign Key
+    position_id = db.Column(db.Integer, db.ForeignKey('position.position_id'), nullable=False, unique=True)
+    division_id = db.Column(db.Integer, db.ForeignKey('division.division_id'), nullable=False, unique=True)
+
+    # Relationship
+    suggestions  = db.relationship('User', backref='suggestion', lazy=True)
+    questions    = db.relationship('User', backref='questions', lazy = True)
+    votes        = db.relationship('User', backref='vote', lazy = True, uselist = False)
+
+class Position(db.Model):
+    position_id     = db.Column(db.Integer, primary_key=True)
+    position_name   = db.Column(db.String(120), nullable=False)
+
+    # relationship
+    positions       = db.relationship('Position', backref='user', lazy=True, uselist=False)
+
+class Division(db.Model):
+    division_id     = db.Column(db.Integer, primary_key=True)
+    division_name   = db.Column(db.String(120), nullable=False)
+
+    # relationship
+    divisions       = db.relationship('Division', backref='user', lazy=True, uselist=False)
+
+class Suggestion(db.Model):
+    suggestion_id     = db.Column(db.Integer, primary_key=True)
+    suggestion_name   = db.Column(db.String(50), nullable=False)
+    suggestion_division   = db.Column(db.String(50), nullable=False)
+
+    #Foreign Key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+
+class Candidate(db.Model):
+    candidate_id     = db.Column(db.Integer, primary_key=True)
+    candidate_name   = db.Column(db.String(50), nullable=False)
+    candidate_vision   = db.Column(db.Text, nullable=False)
+    candidate_mission  = db.Column(db.Text, nullable=False)
+    candidate_blueprint= db.Column(db.String(200), nullable=False)
+    candidate_video    = db.Column(db.String(100), nullable=False)
+
+    #relationship
+    questions    = db.relationship('Candidate', backref='questions', lazy = True)
+    votes        = db.relationship('Candidate', backref='vote', lazy = True)
+
+class Question(db.Model):
+    question_id             = db.Column(db.Integer, primary_key=True)
+    question_time_stamp     = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    question_subject        = db.Column(db.String, nullable=False)
+    question_body           = db.Column(db.Text, nullable=False)
+
+    #Foreign Key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    candidate_id = db.Column(db.Integer, db.ForeignKey('candidate.candidate_id'), nullable=False)
+
+class Vote(db.Model):
+    vote_id             = db.Column(db.Integer, primary_key=True)
+    
+
+    #Foreign Key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False, unique=True)
+    candidate_id = db.Column(db.Integer, db.ForeignKey('candidate.candidate_id'), nullable=False)
 
 def delete():
     if(os.path.exists("db.sqlite3")):
